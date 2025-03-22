eb5604ecb3e6c851e963ce0fa91329bba3083c0f
plcg85 <plcgcpm2@gmail.com>
2025-03-22 18:57:21 +0100

    componente de clase

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,14 +13,14 @@
 <body>
     <div class="max-w-4xl mx-auto px-4">
         <!--<h1>Bienvenido a la página principal</h1>-->
-        <x-alert type="danger" class="mb-4"> <!-- paso de información por los atributos de la alerta-->
+        <x-alert2 type="info" class="mb-4"> <!-- paso de información por los atributos de la alerta-->
             <!--llamando al componente alert views.components.alert.blade.php -->
             <x-slot name="title">
                 <!-- en la variable title se mete el contenido mas abajo que va al componente alert -->
                 Titulo de la alerta
             </x-slot>
             Contenido de la alerta
-        </x-alert>
+        </x-alert2>
         <p>Hola mundo</p>
     </div>
 
