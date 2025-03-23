803a1d0d99a0f0078231d200157d0b7f13c4aa50
plcg85 <plcgcpm2@gmail.com>
2025-03-23 18:45:41 +0100

    route resource

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,26 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 
+
+
+
 Route::get('/', HomeController::class); //no se le pasa el método porque este controlador solo tiene un metodo invoke
+Route::apiResource('posts', PostController::class);
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+//esto era antes de usar Route::resource
+// Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+// Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+// Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+// Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 
-Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+// Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
 
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+// Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 Route::get('/prueba', function () {
 
